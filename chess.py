46cee8e7fb65f46488799818c9406d3beda4e269
Xander T. <150751577+therealnumbers@users.noreply.github.com>
2026-04-16 13:38:29 -0700

    Create chess.py

---
+++ chess.py
@@ -0,0 +1,15 @@
+# this is the third time weve completley restated
+# lets go
+
+import random as ra
+from typing import Tuple
+from enum import Enum
+
+# First: we need a way to view the board and the pieces within
+# Zeroth: we need a way to represent pieces
+# Lets try the bitboard approach
+#
+# -- Board Representation --
+# First imagine a small 2 x 2 board
+# The index I of any one square is 
+#
